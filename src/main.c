dedfa32033b3ee11a454aa3be99b671ee79871da
Adele Hurabielle <adele.hurabielle-claverie@telecom-paristech.fr>
2014-05-18 20:30:18 +0200

    Bootloader with pins assignment for drops. Also replace push button with magic word verification

--- src/main.c
+++ src/main.c
@@ -5,9 +5,9 @@
  * @ingroup dfu_bootloader_api
  * @brief Bootloader project main file.
  *
- * -# Receive start data package. 
- * -# Based on start packet, prepare NVM area to store received data. 
- * -# Receive data packet. 
+ * -# Receive start data package.
+ * -# Based on start packet, prepare NVM area to store received data.
+ * -# Receive data packet.
  * -# Validate data packet.
  * -# Write Data packet to NVM.
  * -# If not finished - Wait for next packet.
@@ -37,33 +37,30 @@
 #include "softdevice_handler.h"
 #include "pstorage_platform.h"
 
-#define BOOTLOADER_BUTTON_PIN           29                                                      /**< Button used to enter SW update mode. */
-#define BOOTLOADER_LED_PIN              13
+#define BOOTLOADER_LED_PIN              8
 
-#define APP_GPIOTE_MAX_USERS            1                                                       /**< Number of GPIOTE users in total. Used by button module and dfu_transport_serial module (flow control). */
+#define APP_GPIOTE_MAX_USERS            1                                                       /**< Number of GPIOTE users in total. Used by dfu_transport_serial module (flow control). */
 
 #define APP_TIMER_PRESCALER             0                                                       /**< Value of the RTC1 PRESCALER register. */
 #define APP_TIMER_MAX_TIMERS            3                                                       /**< Maximum number of simultaneously created timers. */
 #define APP_TIMER_OP_QUEUE_SIZE         4                                                       /**< Size of timer operation queues. */
-
-#define BUTTON_DETECTION_DELAY          APP_TIMER_TICKS(50, APP_TIMER_PRESCALER)                /**< Delay from a GPIOTE event until a button is reported as pushed (in number of timer ticks). */
 
 #define SCHED_MAX_EVENT_DATA_SIZE       MAX(APP_TIMER_SCHED_EVT_SIZE, 0)                        /**< Maximum size of scheduler events. */
 
 #define SCHED_QUEUE_SIZE                20                                                      /**< Maximum number of events in the scheduler queue. */
 
 
-/**@brief Function for error handling, which is called when an error has occurred. 
+/**@brief Function for error handling, which is called when an error has occurred.
  *
- * @warning This handler is an example only and does not fit a final product. You need to analyze 
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
  *          how your product is supposed to react in case of error.
  *
  * @param[in] error_code  Error code supplied to the handler.
  * @param[in] line_num    Line number where the handler is called.
- * @param[in] p_file_name Pointer to the file name. 
+ * @param[in] p_file_name Pointer to the file name.
  */
 void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p_file_name)
-{    
+{
     //nrf_gpio_pin_set(LED_7);
     // This call can be used for debug purposes during application development.
     // @note CAUTION: Activating this code will write the stack to flash on an error.
@@ -83,7 +80,7 @@ void app_error_handler(uint32_t error_code, uint32_t line_num, const uint8_t * p
  *
  * @details This function will be called in case of an assert in the SoftDevice.
  *
- * @warning This handler is an example only and does not fit a final product. You need to analyze 
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
  *          how your product is supposed to react in case of Assert.
  * @warning On assert from the SoftDevice, the system can only recover on reset.
  *
@@ -124,16 +121,6 @@ static void timers_init(void)
 }
 
 
-/**@brief Function for initializing the button module.
- */
-static void buttons_init(void)
-{   
-    nrf_gpio_cfg_sense_input(BOOTLOADER_BUTTON_PIN,
-                             NRF_GPIO_PIN_PULLDOWN,
-                             NRF_GPIO_PIN_SENSE_HIGH);
-}
-
-
 /**@brief Function for dispatching a BLE stack event to all modules with a BLE stack event handler.
  *
  * @details This function is called from the scheduler in the main loop after a BLE stack
@@ -154,7 +141,7 @@ static void sys_evt_dispatch(uint32_t event)
 static void ble_stack_init(void)
 {
     uint32_t err_code;
-    
+
     //SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, true);
     SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_SYNTH_250_PPM, true);
 
@@ -176,7 +163,6 @@ static void scheduler_init(void)
 int main(void)
 {
     uint32_t err_code;
-    bool     bootloader_is_pushed = false;
 
     // This check ensures that the defined fields in the bootloader corresponds with actual
     // setting in the nRF51 chip.
@@ -189,15 +175,13 @@ int main(void)
     leds_init();
     timers_init();
     gpiote_init();
-    buttons_init();
     ble_stack_init();
     scheduler_init();
 
-    bootloader_is_pushed = ((nrf_gpio_pin_read(BOOTLOADER_BUTTON_PIN) == 1)? true: false);
-    
-    if (bootloader_is_pushed || (!bootloader_app_is_valid(DFU_BANK_0_REGION_START)))
+    if ((*(volatile uint32_t *) 0x20003c7c == 0xade1e) || (!bootloader_app_is_valid(DFU_BANK_0_REGION_START)))
     {
         nrf_gpio_pin_set(BOOTLOADER_LED_PIN);
+        *(volatile uint32_t *) 0x20003c7c == 0xffffffff;
 
         // Initiate an update of the firmware.
         err_code = bootloader_dfu_start();
@@ -210,7 +194,7 @@ int main(void)
 
         // Select a bank region to use as application region.
         // @note: Only applications running from DFU_BANK_0_REGION_START is supported.
-        bootloader_app_start(DFU_BANK_0_REGION_START);   
+        bootloader_app_start(DFU_BANK_0_REGION_START);
     }
 
     NVIC_SystemReset();
